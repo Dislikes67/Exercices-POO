1fee3d9213a772328e11eb1ad47e4132b795b3f7
Mathias.Rudolf <matt67creativ@hotmail.fr>
2024-06-19 11:58:38 +0200

    Debut exo3

---
+++ Exo3.php
@@ -0,0 +1,22 @@
+<h1>Exercice 3</h1>
+
+<p>Créer une classe Personne (nom,prénom et date de naissance).<br> Instancier 2 personnes et afficher leurs informations.</p>
+
+
+<?php
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
