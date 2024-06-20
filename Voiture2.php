bb7b945dca10692935255fa450d3a4bcf827dfb9
Mathias.Rudolf <matt67creativ@hotmail.fr>
2024-06-20 16:24:40 +0200

    Fin voiture2

---
+++ Voiture2.php
@@ -0,0 +1,41 @@
+<?php
+
+class Voiture2 {
+
+    protected string $marque;
+    protected string $modele;
+
+    public function __construct(string $marque, string $modele) {
+        $this->marque = $marque;
+        $this->modele = $modele;
+    }
+
+    public function getMarque() : string {
+        return $this->marque;
+    }
+
+    public function getModele() : string {
+        return $this->modele;
+    }
+
+
+
+    public function setMarque(string $marque){
+        return $this->marque;
+    }
+    
+    public function setModele(string $modele) {
+        $this->modele = $modele;
+    }
+
+
+    public function afficherInfos() {
+        return  "<p>Nom et modèle du véhicule : $this->marque $this->modele</p>";
+    }
+
+    public function __toString() {
+        return "$this->marque $this->modele";
+    }
+}
+
+?>
