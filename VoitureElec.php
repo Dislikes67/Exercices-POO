85baae3d56098502e31e4fefc17136d982af675d
Mathias.Rudolf <matt67creativ@hotmail.fr>
2024-06-20 15:58:00 +0200

    update exovoiture

---
+++ VoitureElec.php
@@ -0,0 +1,31 @@
+<h1>Exercice 5</h1>
+
+<p>Créer une classe Voiture possédant 2 propriétés (marque et modèle) ainsi qu'une classe VoitureElec qui hérite de la classe Voiture et qui a une propriété supplémentaire (autonomie).</p>
+
+<?php
+
+class VoitureElec {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
