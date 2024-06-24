0934d0bca13a2468d9f58e95f6065d8d479e05e9
Mathias Rudolf <matt67creativ@hotmail.fr>
2024-06-24 10:08:45 +0200

    fin exo livre

--- exolivre/Auteur.php
+++ exolivre/Auteur.php
@@ -1,13 +1,16 @@
 <?php
 
-class Auteur{
+class Auteur {
     private string $prenom;
     private string $nom;
+// Création du tableau
+    private array $bibliographie;
+
 
     public function __construct(string $prenom, string $nom){
-
         $this->prenom = $prenom;
         $this->nom = $nom;
+        $this->bibliographie = [];
     }
                                                         
     public function getPrenom() : string{
@@ -18,6 +21,9 @@ class Auteur{
         return $this->nom;
     }
 
+    public function getBiblio(): array{
+        return $this->bibliographie;
+    }
 
 
     public function setPrenom(string $prenom){
@@ -28,15 +34,22 @@ class Auteur{
         $this->nom = $nom;
     }
 
+    public function setBiblio(array $bibliographie){
+        $this->bibliographie = $bibliographie;
+        return $this;
+    }
 
-    public function afficherBibliographie(array $livres): string {
-        $result = "Livres de" . $this->prenom ." ". $this->nom . " :<br>";
-        foreach ($livres as $livre) {
-            if ($livre->getAuteur() == $this){
+//Méthode qui permet d'ajouter un objet livre au tableau dés qu'il est crée avec l'objet auteur en paramètre
+    public function addLivre(Livre $livre) {
+        $this->bibliographie[] = $livre;
+    }
+
+    public function afficherBiblio(): string {
+        $result = "<h1>Livres de $this</h1><br><br>";
+        foreach ($this->bibliographie as $livre){
                 $result .= $livre . "<br>"; 
             
             }
-        }
         return $result;
     }
 
