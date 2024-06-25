ca04839b7f3ed89f1d6718fc8d984772f96b9813
Mathias Rudolf <matt67creativ@hotmail.fr>
2024-06-25 16:59:58 +0200

    minor changes

--- exoBanque/Compte.php
+++ exoBanque/Compte.php
@@ -7,7 +7,7 @@ class Compte {
     private Titulaire $titulaire;
 
 
-    public function __construct(string $libelle, float $soldeInit, string $devise, Titulaire $titulaire){
+    public function __construct(string $libelle, float $soldeInit, string $devise, Titulaire $titulaire) {
         $this->libelle = $libelle;
         $this->soldeInit = $soldeInit;
         $this->devise = $devise;
